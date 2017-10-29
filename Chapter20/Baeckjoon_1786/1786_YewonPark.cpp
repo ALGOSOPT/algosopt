6c2dc21ba2758651451c92efb558014e946e3ebc
YEWON <dpdnjs1222@naver.com>
2017-10-29 18:44:15 +0900

    1786 수정(상조오빠가 알려준 코드로)

--- Chapter20/Baeckjoon_1786/1786_YewonPark.cpp
+++ Chapter20/Baeckjoon_1786/1786_YewonPark.cpp
@@ -1,54 +1,42 @@
 #include<iostream>
 #include<vector>
-#include<string>
-#include<algorithm>
 
 using namespace std;
-//부분일치테이블 생성하는 함수
-vector<int> PartialMatch(const string& input) {
-	int n = input.size();
-	vector<int> v(n, 0);//얘가 부분일치테이블
 
-	int begin = 1, matched = 0;
+vector<int> getPartial(string p) {
 
-	while (begin + matched<n) {
-		if (input[begin + matched] == input[matched]) {
-			++matched;//일치하는구나!
-			v[begin + matched - 1] = matched;//갱신
-		}
-		else {
-			if (matched == 0)//하나도 일치하지 않으면
-				++begin;
-			else {//하나 이상 일치한 후 불일치 발생하면 begin이랑 matched를 바꿔준다(kmp)
-				begin += matched - v[matched - 1];
-				matched = v[matched - 1];
-			}
+	int m = p.size();
+	vector<int> pi(m, 0);
+	
+	int matched = 0;
+
+	for (int i = 1; i < m; i++) {
+		while (matched > 0 && p[i] != p[matched])
+			matched = pi[matched - 1];
+
+		if (p[i] == p[matched]) {
+			matched++;
+			pi[i] = matched;
 		}
 	}
-
-	return v;
+	return pi;
 }
 
-vector<int> kmp(const string& H, const string& N) {
-	int n = H.size(), m = N.size();
+vector<int> kmp(string H, string N) {
+	int m = H.size(); int n = N.size();
+	vector<int> pi = getPartial(N);
 
 	vector<int> ret;
-	vector<int> pi = PartialMatch(N);
 
-	int begin = 0, matched = 0;
+	int matched = 0;
+	for (int i = 0; i < m; i++) {
+		while (matched > 0 && H[i] != N[matched])
+			matched = pi[matched - 1];
 
-	while (begin <= n - m) {
-		if (matched<m && H[begin + matched] == N[matched]) {
-			++matched;
-			if (matched == m) {
-				ret.push_back(begin);
-			}
-		}
-		else {
-			if (matched == 0)
-				++begin;
-			else {
-				begin += matched - pi[matched - 1];
+		if (H[i] == N[matched]) {
+			matched++;
+			if (matched == n) {
+				ret.push_back(i - n + 1);
 				matched = pi[matched - 1];
 			}
 		}
@@ -62,13 +50,15 @@ int main() {
 	string P = "";
 
 	char input[1000000];
-	cin.getline(input, 1000000);
 	
+	cin.getline(input, 1000000);
+
 	for (int i = 0; input[i] != NULL; i++) {
 		T += input[i];
 	}
-	
+
 	cin.getline(input, 1000000);
+	
 	for (int i = 0; input[i] != NULL; i++) {
 		P += input[i];
 	}
