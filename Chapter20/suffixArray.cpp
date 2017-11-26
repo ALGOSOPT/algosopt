383a2987614d12f6590adb34270bc61fc61b82bd
JISUJUNG <jjs941114@gmail.com>
2017-11-26 13:36:41 +0900

    sss

--- Chapter20/suffixArray.cpp
+++ Chapter20/suffixArray.cpp
@@ -17,12 +17,15 @@ vector<int> SA;
 vector<int> group;
 
 //접미사 비교에 사용할 함수
+// 같은 그룹 0, 다른 그룹 1
+// 시간 복잡도가 O(1)
 bool cmp(int i, int j){
 
-	//먼저 자신의 위치의 문자를 비교
+	// 먼저 자신의 위치의 문자를 비교
 	if(group[i] != group[j]) return group[i] < group[j];
 
-	//문자가 같으면 d칸 뒤의 문자끼리 비교
+	// 문자가 같으면 d칸 뒤의 문자끼리 비교
+    //
 	i += d;
 	j += d;
 	return (i < N && j < N) ? (group[i] < group[j]) : (i > j);
@@ -37,10 +40,12 @@ void constructSA(){
 	for(int i = 0; i < N; i++){
 		SA[i] = i;
 		//제일 첫 번째 루프에서는 제자리 문자로 비교
+        // 접미사의 첫 글자로 그루핑한다.
 		group[i] = S[i];
 	}
 
-	for(d = 1; ; d *= 2){
+	for(d = 1; ; d *= 2) {
+        // 그룹이 같을때와
 		sort(SA, SA + N, cmp);
 		//newGroup : 새로운 그룹 번호
 		vector<int> newGroup(N, 0);
@@ -49,7 +54,7 @@ void constructSA(){
 		for(int i = 0; i < N - 1; i++)
 			newGroup[i + 1] = newGroup[i] + cmp(SA[i], SA[i + 1]);
 
-		//group배열을 newGroup 배열로 대체
+		//group 배열을 newGroup 배열로 대체
 		for(int i = 0; i < N; i++)
 			group[SA[i]] = newGroup[i];
 
