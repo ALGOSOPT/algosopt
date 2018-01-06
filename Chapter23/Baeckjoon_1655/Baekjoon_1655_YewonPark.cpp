9602ea1a9bf9ab03468298e75e1c5f6798a68acd
YEWON\yewon <dpdnjs12222@gmail.com>
2018-01-07 01:42:14 +0900

    success

--- Chapter23/Baeckjoon_1655/Baekjoon_1655_YewonPark.cpp
+++ Chapter23/Baeckjoon_1655/Baekjoon_1655_YewonPark.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 priority_queue<int, vector<int>, less<int> > maxHeap;
 priority_queue<int, vector<int>, greater<int> > minHeap;
-int arr[200000];
+int arr[100000];
 
 //중간값을 출력해야하므로 정수를 반환 
 int getMedian(int n){
@@ -36,24 +36,14 @@ int getMedian(int n){
 
 int main(){
 	
-	int c;
-	scanf("%d", &c);
-	for(int i=0; i<c; i++){
-		int n, a, b;
-		scanf("%d %d %d", &n, &a, &b);
+		int n;
+		scanf("%d", &n);
 		
 		maxHeap = priority_queue<int, vector<int>, less<int> >();
 		minHeap = priority_queue<int, vector<int>, greater<int> >();
 		
-		arr[0] = 1983;
-		for(int i=1; i<n; i++)
-			arr[i] = (arr[i-1]*(long long)a + b)%20090711; 
-			
-		int result = 0;
 		for(int i=0; i<n; i++){
-			result += getMedian(i);
-			result %= 20090711;
+			scanf("%d", &arr[i]);
+			printf("%d\n", getMedian(i));	
 		}
-		printf("%d\n",result); 
-	}
 }
